fa4b484994da553db0c03d460c9757ecd03f789d
Vikhyath <gthg65@gmail.com>
2016-09-16 10:45:57 +0530

    Reorganize files into neat directories

---
+++ RLandApp-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RLandApp-Bridging-Header.h
+//  
+//
+//  Created by Vikhyath on 9/16/16.
+//
+//
+
+#ifndef RLandApp_Bridging_Header_h
+#define RLandApp_Bridging_Header_h
+
+
+#endif /* RLandApp_Bridging_Header_h */
--- RLandApp/Tabbar/theTabBarViewController.h
+++ RLandApp/Tabbar/theTabBarViewController.h
@@ -11,7 +11,6 @@
 
 @interface theTabBarViewController : UITabBarController<UITabBarControllerDelegate>
 
-
     {
     @public bool tf;
     }
